cb4568035728b2a6a176e29da73b0e30ece81cc9
AshishGaur11 <77341193+AshishGaur11@users.noreply.github.com>
2021-04-02 20:34:18 +0530

    Update classforcomplex.py

--- classforcomplex.py
+++ classforcomplex.py
@@ -55,3 +55,46 @@ v1 = Vector(2,10)
 v2 = Vector(5,2)
 v3 = v1 + v2
 Vector.put(v3)
+
+
+
+
+
+
+
+
+*************************************************************************************************
+class complex:
+
+    def __init__(self,x,i):
+        self.x = x
+        self.i = i
+    
+    def get(self):
+        self.x = int(input("enter real number"))
+        self.i = int(input("enter complex number"))
+
+    def put(self):
+        print(self.x,'+',self.i,'i')
+
+    def __add__(self,other):
+        v = self.x + other.x
+        w = self.i + other.i
+        return complex(v,w)
+    
+    def __mul__(self,other):
+        return complex(self.x * other.x, self.i * other.i)
+        
+  
+if __name__ == "__main__":
+    a = complex(0,0)
+    b = complex(0,0)
+    c = complex(0,0)
+    d = complex(0,0)
+    complex.get(a)
+    complex.get(b)
+    c = complex.__add__(a,b)
+    d = a * b
+    complex.put(c)
+    complex.put(d)
+  
