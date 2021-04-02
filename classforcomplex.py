05fba789a9d87734cd383729bfb47529842e40b7
AshishGaur11 <77341193+AshishGaur11@users.noreply.github.com>
2021-04-02 19:17:53 +0530

    Update classforcomplex.py

--- classforcomplex.py
+++ classforcomplex.py
@@ -6,81 +6,24 @@ class complex:
     def __init__(self,x,i):
         self.x = x
         self.i = i
-        
+    
     def get(self):
         self.x = int(input("enter real number"))
         self.i = int(input("enter complex number"))
-        
+
     def put(self):
         print(self.x,'+',self.i,'i')
-    
+
     def __str__(self):
         return self.x,self.i
-    
+
     def __add__(self,other):
-        return complex(self.x + other.x, self.i + other.i)
-        
-        
-        
+      return complex(self.x + other.x, self.i + other.i)
+  
 if __name__ == "__main__":
-    complex.get(a)
-    complex.get(b)
-    c = a + b
-    complex.put(c)
-    
-    
-    #this is better
-    class complex:
-
-    def __init__(self,x,i):
-        self.x = x
-        self.i = i
-        
-    def get(self):
-        self.x = int(input("enter real number"))
-        self.i = int(input("enter complex number"))
-        
-    def put(self):
-        print(self.x,'+',self.i,'i')
-    
-    def __str__(self):
-        return self.x,self.i
-    
-    def __add__(self,other):
-        self.x = self.x + other.x
-        self.x = self.i + other.i        
-        
-        
-if __name__ == "__main__":
-    complex.get(a)
-    complex.get(b)
-    c = a + b
-    complex.put(c)
-    
-  #or 
-class complex:
-
-    def __init__(self,x,i):
-        self.x = x
-        self.i = i
-    
-    def get(self):
-        self.x = int(input("enter real number"))
-        self.i = int(input("enter complex number"))
-
-    def put(self):
-        print(self.x,'+',self.i,'i')
-
-    def __str__(self):
-        return self.x,self.i
-
-    def __add__(self,r,e):
-        self.x = r.self.x + e.self.x
-        self.i = r.self.i + e.self.i
-
-        
-        
-if __name__ == "__main__":
+    a = complex(0,0)
+    b = complex(0,0)
+    c = complex(0,0)
     complex.get(a)
     complex.get(b)
     c = a + b
