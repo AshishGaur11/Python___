4b276d148c3d3bdbfd61458130f2f635117a1071
AshishGaur11 <77341193+AshishGaur11@users.noreply.github.com>
2021-04-02 15:13:25 +0530

    Update classforcomplex.py

--- classforcomplex.py
+++ classforcomplex.py
@@ -57,4 +57,31 @@ if __name__ == "__main__":
     c = a + b
     complex.put(c)
     
+  #or 
+class complex:
+
+    def __init__(self,x,i):
+        self.x = x
+        self.i = i
     
+    def get(self):
+        self.x = int(input("enter real number"))
+        self.i = int(input("enter complex number"))
+
+    def put(self):
+        print(self.x,'+',self.i,'i')
+
+    def __str__(self):
+        return self.x,self.i
+
+    def __add__(self,r,e):
+        self.x = r.self.x + e.self.x
+        self.i = r.self.i + e.self.i
+
+        
+        
+if __name__ == "__main__":
+    complex.get(a)
+    complex.get(b)
+    c = a + b
+    complex.put(c)
