415208c68f0b2ec0d01b13bc7efb84e3c19a3f44
AshishGaur11 <77341193+AshishGaur11@users.noreply.github.com>
2021-07-18 14:26:08 +0530

    Update infinite_value_input_process.py

--- infinite_value_input_process.py
+++ infinite_value_input_process.py
@@ -4,3 +4,14 @@ def ditw(*arg):
         m[i] = arg[i] 
     m.sort()
     print(m)
+    
+    
+    
+    
+def addition(n):
+    return n * n
+  
+
+numbers = (1, 2, 3, 4)
+result = map(addition, numbers)
+print(list(result))
